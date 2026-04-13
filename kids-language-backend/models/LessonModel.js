bd40f98d73c1777473a61ac23f573c5dfd2d381b
Cindy Katua <cindy.wendoh@gmail.com>
2026-04-13 15:25:25 +0300

    Added backend (users, languages, lessons)

---
+++ kids-language-backend/models/LessonModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+console.log("LESSON MODEL FILE LOADED");
+
+const lessonSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    required: true
+  },
+  languageId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Language",
+    required: true
+  }
+}, { timestamps: true });
+
+const Lesson = mongoose.model("Lesson", lessonSchema);
+
+console.log("Lesson model created:", typeof Lesson);
+
+module.exports = Lesson;
