74e55a947aed27042bb9291e8c0dcef43eadf8df
Cindy Katua <cindy.wendoh@gmail.com>
2026-04-21 20:04:36 +0300

    Restored working dynamic lessons page

--- languages/english/lessons.js
+++ languages/english/lessons.js
@@ -1,34 +1,3 @@
-/*const colorLesson = document.getElementById("colorLesson");
-
-// Go to Color Hunt lesson
-colorLesson.addEventListener("click", () => {
-  window.location.href = "colors.html";
-});
-
-// Shapes
-shapeLesson.addEventListener("click", () => {
-  window.location.href = "shapes.html";
-});
-
-// Others (optional placeholders for now)
-alphabetLesson.addEventListener("click", () => {
-  window.location.href = "alphabet.html";
-});
-
-animalLesson.addEventListener("click", () => {
-  window.location.href = "animals.html";
-});
-
-fruitLesson.addEventListener("click", () => {
-   window.location.href = "fruit.html";
-});
-
-
-// Back to menu
-function goBack() {
-  window.location.href = "../menu.html";
-}*/
-
 const lessonContainer = document.getElementById("lessonContainer");
 
 function getLessonEmoji(title) {
