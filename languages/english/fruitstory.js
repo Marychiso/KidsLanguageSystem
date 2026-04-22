80c0c5c4970bc0a9dff284a03cf8c03a3c2b6a7e
Marychiso <ifedmaryc@gmail.com>
2026-04-22 21:35:21 +0300

    stories

---
+++ languages/english/fruitstory.js
@@ -0,0 +1,43 @@
+let current = 1;
+
+// GET TEXT
+function getCurrentText() {
+  return document.querySelector("#screen" + current + " .story-text").textContent;
+}
+
+// VOICE
+function playVoice(text) {
+  window.speechSynthesis.cancel();
+  const speech = new SpeechSynthesisUtterance(text);
+  speech.rate = 0.8;
+  speech.lang = "en-US";
+  window.speechSynthesis.speak(speech);
+}
+
+// NEXT
+function next() {
+  document.getElementById("screen" + current).classList.remove("active");
+  current++;
+  document.getElementById("screen" + current).classList.add("active");
+
+  playVoice(getCurrentText());
+}
+
+// BACK
+function back() {
+  document.getElementById("screen" + current).classList.remove("active");
+  current--;
+  document.getElementById("screen" + current).classList.add("active");
+
+  playVoice(getCurrentText());
+}
+
+// GO BACK TO STORIES
+function goStories() {
+  window.location.href = "stories.html"; // adjust if needed
+}
+
+// AUTO VOICE
+window.onload = () => {
+  playVoice(getCurrentText());
+};
